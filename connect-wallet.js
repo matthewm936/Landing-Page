006c9b74b96f45a66915e52294662917f41c7ec8
matthewm936 <98236645+matthewm936@users.noreply.github.com>
2023-02-07 19:14:32 -0700

    connect wallet linked to metamask if not installed

--- connect-wallet.js
+++ connect-wallet.js
@@ -1,6 +1,8 @@
 //check if metamask is installed
 window.onload = function() {
     if(typeof window.ethereum == "undefined") {
-        document.getElementById('connect').innerHTML = "download metamask";
+        document.getElementById('connect')
+        .innerHTML = 
+        "<a href='https://metamask.io/download/' style= 'color: white'>download metamask</a>";
     }
 };
