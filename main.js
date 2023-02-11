563bdafea4b32b2641249583f66a4e40581c4773
matthewm936 <98236645+matthewm936@users.noreply.github.com>
2023-02-10 18:34:19 -0700

    working metamask connection api call

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-async function main() {
-    const ethereumButton = document.querySelector('connect-wallet');
+import detectEthereumProvider from '@metamask/detect-provider'
 
-    ethereumButton.addEventListener('click', () => {
-    //Will Start the metamask extension
-    ethereum.request({ method: 'eth_requestAccounts' });
-    });
+const provider = await detectEthereumProvider()
+
+if (provider) {
+
+  console.log('Ethereum successfully detected!')
+
+  // From now on, this should always be true:
+  // provider === window.ethereum
+
+  // Access the decentralized web!
+
+  // Legacy providers may only have ethereum.sendAsync
+  const chainId = await provider.request({
+    method: 'eth_chainId'
+  })
+} else {
+
+  // if the provider is not detected, detectEthereumProvider resolves to null
+  console.error('Please install MetaMask!', error)
 }
